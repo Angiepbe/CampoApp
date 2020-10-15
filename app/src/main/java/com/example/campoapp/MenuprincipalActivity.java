2ab9a0799e7e7abe934e1403d12e91e54dc73f98
angiepbe <apbe2000@gmail.com>
2020-10-14 20:49:27 -0500

    Version Inicial

--- app/src/main/java/com/example/campoapp/MenuprincipalActivity.java
+++ app/src/main/java/com/example/campoapp/MenuprincipalActivity.java
@@ -2,13 +2,22 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MenuprincipalActivity extends AppCompatActivity {
+ public class MenuprincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuprincipal);
+
     }
+
+     public void AsisVirtual (View view ){
+         Intent siri= new Intent(this,AsistenteVirtual.class);//Generar instancia entre pantallar
+         //Envio de info entre pantallas
+         startActivity(siri);//Inicializar objeto
+     }
 }
